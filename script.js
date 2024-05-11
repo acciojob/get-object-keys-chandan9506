ca525eba777b72d4f37eac15da5c3f9574b1b7e0
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-05-11 10:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let student = {
+	"name":"Chandan",
+}
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Test the implementation
+console.log(student.getKeys()); // Output: ["name"]
